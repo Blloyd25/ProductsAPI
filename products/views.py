babb544400f69da36279f179ef275e18379cccff
benjamin lloyd <benjamin.a.lloyd25@gmail.com>
2022-02-15 14:37:12 -0500

    created new view function for views and urls

--- products/views.py
+++ products/views.py
@@ -1,9 +1,12 @@
+
 from urllib import response
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
+from products import serializers
 
 
 @api_view(['GET','POST'])
@@ -20,5 +23,16 @@ def products_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response (serializer.data, status=status.HTTP_201_CREATED)
+@api_view(['GET'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(Product)
+        return Response(serializer.data)
+
+
+    except Product.DoesNotExist:
+        return Response (status=status.HTTP_404_NOT_FOUND)
+
        
 
